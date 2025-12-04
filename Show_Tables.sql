e513d8918bca1ec60c5b1830ad6fcb1ffe6a46f9
.mario?????? <125089516+benoitlin@users.noreply.github.com>
2025-12-04 15:20:35 +0100

    Delete Show_Tables.sql

--- Show_Tables.sql
+++
@@ -1,9 +0,0 @@
-USE projet_rest;
-
-SHOW TABLES;
-SELECT * FROM films;
-SELECT * FROM programmation_seance;
-SELECT * FROM salles;
-SELECT * FROM acteurs;
-SELECT * FROM acteur_film;
-SELECT * FROM avis;
